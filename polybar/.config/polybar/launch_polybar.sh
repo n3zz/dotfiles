f9449d807451308c40673c3265a3c93546442569
ary <n3xi@protonmail.com>
2026-07-02 19:47:27 +0100

    polybar: tried to fix the gpu-record indicator just flickering for some reason

--- polybar/.config/polybar/launch_polybar.sh
+++ polybar/.config/polybar/launch_polybar.sh
@@ -1,8 +1,13 @@
-pkill polybar
-if type "xrandr" ; then
-	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		MONITOR=$m polybar --reload main &
-	done
+#!/usr/bin/env bash
+
+# Kill any existing polybar instances before launching fresh ones
+killall -q polybar
+while pgrep -x polybar >/dev/null; do sleep 0.2; done
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
 else
-	polybar --reload main &
+  polybar --reload main &
 fi
